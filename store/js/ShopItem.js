b2b8265b9e02c480dc4cceae062b102ce2a20447
EkaterinaBudilova <ekaterinazh1@mail.ru>
2018-04-21 08:47:43 +1200

    v.3

--- store/js/ShopItem.js
+++ store/js/ShopItem.js
@@ -1,14 +1,17 @@
-const ShopItem = (
-  <div className='main-content'>
-    <h2>{item.brand}</h2>
-    <h1>{item.title}</h1>
-    <h3>{item.description}</h3>
-    <div className='description'> Mens minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length. </div>
-    <div className='highlight-window  mobile'><div className='highlight-overlay'></div></div>
-    <div className='divider'></div>
-    <div className='purchase-info'>
-    <div className='price'>{item.price}</div>
-    <button>Добавить в корзину</button>
-  </div>
-</div>
-);
+const ShopItem = function(props) {
+  const {brand, title, description, price} = props;
+  return (
+    <div className='main-content'>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className='description'> Mens minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent.   Slim fit with clean, straight shape. Above-knee length. </div>
+      <div className='highlight-window  mobile'><div className='highlight-overlay'></div></div>
+      <div className='divider'></div>
+      <div className='purchase-info'>
+      <div className='price'>{price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
